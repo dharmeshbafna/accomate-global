43bd27d89d4e3a927c804d56801dcd58c2525eca
clientanriyo <client@anriyo.com>
2024-05-09 18:34:46 +0530

    9.5.24

---
+++ src/components/contact.js
@@ -0,0 +1,20 @@
+'use client'
+
+import { useState } from "react"
+
+export const Contact = () =>{
+    
+    const [fd, setFd] = useState({
+        name: '',
+        email: '',
+        message: '',
+        sub: '',
+        phone: ''
+    });
+
+    return (
+        <div className="">
+
+        </div>
+    )
+}
